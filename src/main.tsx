10bf2104d0eef611bd309123ebfcd2717406b69c
Sasha-Polgar <sasha.polgar@hotmail.com>
2023-12-04 12:53:46 +0100

    :sparkles: feat: change App component for RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
+import { RouterProvider } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import theme from './styles/theme';
-
-import App from './components/App/App';
+import { router } from './router';
 
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <MantineProvider defaultColorScheme="dark" theme={theme}>
-      <App />
+      <RouterProvider router={router} />
     </MantineProvider>
   </React.StrictMode>
 );
