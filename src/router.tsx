c7f616f43921e289ddfbaa2a99ebc4128b6c32ee
Sasha-Polgar <sasha.polgar@hotmail.com>
2023-12-04 12:55:45 +0100

    :sparkles: feat: add path event/:slug

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,23 @@
+/* eslint-disable import/prefer-default-export */
 import { createBrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
-import Error from '.';
+import Error from './components/Error/Error';
+import Home from './components/Home/Home';
+import Event from './components/Event/Event';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <Error />, // TODO add an error page
+    errorElement: <Error />, // TODO edit the error component. Needs Header and Navbar
     children: [
       {
         path: '/',
         element: <Home />,
+      },
+      {
+        path: 'event/:slug',
+        element: <Event />,
       },
     ],
   },
