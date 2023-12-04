414dee402831a5a693f48326ca1ae710d848b46f
Sasha-Polgar <sasha.polgar@hotmail.com>
2023-12-04 13:50:47 +0100

    :sparkles: feat: add register and sign-in paths to router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,8 @@ import Error from './routes/Error/Error';
 import Home from './routes/Home/Home';
 import Event from './routes/Event/Event';
 import Profile from './routes/Profile/Profile';
+import Register from './routes/Register/Register';
+import SignIn from './routes/SignIn/SignIn';
 
 export const router = createBrowserRouter([
   {
@@ -28,7 +30,16 @@ export const router = createBrowserRouter([
       {
         // route for the connected user to see researched user's profile
         path: 'profile/:pseudo',
+        // render the same Profile component with some conditional elements or a new SearchedProfile component
         element: <Profile />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'sign-in',
+        element: <SignIn />,
       },
     ],
   },
