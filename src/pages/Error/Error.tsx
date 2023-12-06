94d79e077f7132e9130e3a535259beb809f104c7
Sasha-Polgar <sasha.polgar@hotmail.com>
2023-12-06 12:07:24 +0100

    :sparkles: feat: add basic 404 page with mantine and an empty Error.scss file for futur styling

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,5 +1,23 @@
+import { Button, Container, Flex, Text, Title } from '@mantine/core';
+
 function Error() {
-  return <div>Error</div>;
+  return (
+    <Container fluid className="error" style={{ height: '100vh' }}>
+      <Flex
+        gap="md"
+        justify="center"
+        align="center"
+        direction="column"
+        style={{ height: '100%' }}
+      >
+        <Title order={1}>404 ERROR</Title>
+        <Text>Tu sembles t&apos;être perdu</Text>
+        <Button component="a" href="/">
+          VERSUS Home
+        </Button>
+      </Flex>
+    </Container>
+  );
 }
 
 export default Error;
