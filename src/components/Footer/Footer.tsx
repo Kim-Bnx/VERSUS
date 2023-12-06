adabef6ad3dd233870abe744056d6b996294b8c1
Kimberley Bonix <kimberley.bonix@oclock.school>
2023-12-06 14:37:14 +0100

    :sparkles: feat: install footer and create new footer pages

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,26 @@
+import { Anchor, Box, Flex, Text, Title } from '@mantine/core';
 import './Footer.scss';
 
 function Footer() {
-  return <p>oui</p>;
+  return (
+    <div className="footer full-width content-grid">
+      <Flex justify="space-between" align="end">
+        <Box className="footer__versus">
+          <Title order={1}>Versus</Title>
+          <Text size="md">
+            Faîtes vivre vos communautés en rassemblant les joueurs en un seul
+            endroit.
+          </Text>
+        </Box>
+
+        <Flex gap="md">
+          <Anchor href="/about">A propos</Anchor>
+          <Anchor href="/contact">Contact</Anchor>
+          <Anchor href="/terms">Mention légales</Anchor>
+        </Flex>
+      </Flex>
+    </div>
+  );
 }
 
 export default Footer;
