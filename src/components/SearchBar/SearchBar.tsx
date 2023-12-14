51ec1e40baa4c800f4b54b34ac8c7fd8b10d0722
Sasha-Polgar <sasha.polgar@hotmail.com>
2023-12-14 08:29:35 +0100

    :sparkles: feat: add url to data to redirect when chosen

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,24 +1,51 @@
 import { Autocomplete, Box } from '@mantine/core';
 import './SearchBar.scss';
 import { IconSearch } from '@tabler/icons-react';
+import { useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { changeSearchInputValue, search } from '../../store/reducers/search';
 
 function SearchBar() {
   const icon = <IconSearch />;
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const searchTerm = useAppSelector((state) => state.search.searchTerm);
   const errorMsg = useAppSelector((state) => state.search.error);
   const data = useAppSelector((state) => state.search.searchResults);
 
-  const events = data.events.map((event) => event.title);
-  const users = data.users.map((user) => user.username);
-  const teams = data.teams.map((team) => team.name);
+  const events = data.events.map((event) => ({
+    label: event.title,
+    value: event.title,
+    url: `/event/${event.title_slug}`,
+  }));
+  const users = data.users.map((user) => ({
+    label: user.username,
+    value: user.username,
+    url: `/profile/${user.username}`,
+  }));
+  const teams = data.teams.map((team) => ({
+    label: team.name,
+    value: team.name,
+    url: `/team/${team.id}`,
+  }));
+
+  const findItemByUrl = (value: string) => {
+    // Combine all items into one array
+    const allItems = [...events, ...users, ...teams];
+
+    // Find the item that has this value
+    return allItems.find((item) => item.label === value);
+  };
 
   const handleChangeSearchValue = (newValue: string) => {
     dispatch(changeSearchInputValue(newValue));
     dispatch(search(newValue));
+
+    const selectedItem = findItemByUrl(newValue);
+    if (selectedItem && selectedItem.url) {
+      navigate(selectedItem.url);
+    }
   };
 
   return (
