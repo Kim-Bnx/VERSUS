78eb96b37205c83b69e6d449c3671df094169d8e
Sasha-Polgar <sasha.polgar@hotmail.com>
2023-12-20 07:04:30 +0100

    :sparkles: feat: make the favorite button visible only for connected user

--- src/components/Element/FavoriteBtn/index.tsx
+++ src/components/Element/FavoriteBtn/index.tsx
@@ -3,27 +3,30 @@ import { UnstyledButton } from '@mantine/core';
 import { IconHeart } from '@tabler/icons-react';
 
 import './index.scss';
+import { useAppSelector } from '../../../hooks/redux';
 
 function FavoriteBtn() {
   const [isHovered, setIsHovered] = useState(false);
+  const isConnected = useAppSelector((state) => state.login.isConnected);
 
   const handleFavoriteHover = () => {
     setIsHovered(!isHovered);
   };
-
-  return (
-    <UnstyledButton
-      className="button--favorite"
-      onMouseEnter={handleFavoriteHover}
-      onMouseLeave={handleFavoriteHover}
-    >
-      <IconHeart
-        stroke={1.5}
-        fill={isHovered ? 'rgb(216, 60, 60)' : '#0c0c0c'}
-        color={isHovered ? 'rgb(216, 60, 60)' : '#929292'}
-      />
-    </UnstyledButton>
-  );
+  if (isConnected) {
+    return (
+      <UnstyledButton
+        className="button--favorite"
+        onMouseEnter={handleFavoriteHover}
+        onMouseLeave={handleFavoriteHover}
+      >
+        <IconHeart
+          stroke={1.5}
+          fill={isHovered ? 'rgb(216, 60, 60)' : '#0c0c0c'}
+          color={isHovered ? 'rgb(216, 60, 60)' : '#929292'}
+        />
+      </UnstyledButton>
+    );
+  }
 }
 
 export default FavoriteBtn;
